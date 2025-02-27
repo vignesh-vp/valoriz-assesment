d6686dddeefb85e2ecc30bfcf50ce8052caa400e
vignesh-vp <vigneshvp.19@gmail.com>
2025-02-28 02:12:27 +0530

    env from configmap

--- menu-frontend/next.config.js
+++ menu-frontend/next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "standalone",  // Ensure output is in standalone mode for production
+    output: "standalone", // ✅ Required for standalone mode
   
+    // Dynamically rewrite API requests based on the environment variable
     async rewrites() {
+      const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:32002"; // Fallback if the environment variable is not set
       return [
         {
           source: "/api/:path*",
-          destination: process.env.NEXT_PUBLIC_API_BASE_URL + "/api/:path*", // Dynamically set destination
+          destination: `${apiBaseUrl}/api/:path*`, // Rewrite to dynamic API base URL
         },
       ];
     },
