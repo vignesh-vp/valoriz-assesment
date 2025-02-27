d26c68b1a5e3db7f0d6584f413ddbc4b612dea8a
vignesh-vp <vigneshvp.19@gmail.com>
2025-02-27 22:50:17 +0530

    env from configmap

--- menu-frontend/next.config.js
+++ menu-frontend/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
