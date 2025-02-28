b150cba85c0701575a62e4f709c02ec0bf6fed27
vignesh-vp <vigneshvp.19@gmail.com>
2025-02-28 10:09:56 +0530

    env from configmap

--- menu-frontend/start.sh
+++ menu-frontend/start.sh
@@ -3,5 +3,5 @@
 # Use the environment variable in your start script
 echo "Starting Next.js app with API URL: ${NEXT_PUBLIC_API_BASE_URL}"
 
-# Start the Next.js app, using the API URL dynamically from the environment
-NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL} next start
+# Run the Next.js app using npm run start, which ensures the local binary is used
+NEXT_PUBLIC_API_BASE_URL=${NEXT_PUBLIC_API_BASE_URL} npm run start
